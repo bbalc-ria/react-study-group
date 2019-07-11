72c020d069a1f34804047b3845ab829a8598da6b
ria-mcucerzan <mcucerzan@riasolutionsgroup.com>
2019-07-11 13:44:20 +0300

    handle change method on parent

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,24 +1,12 @@
 import React from 'react';
 
 class Item extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { completed: this.props.completed };
-
-    this.handleChange = this.handleChange.bind(this);
-
-  };
-
-  handleChange = (event) => {
-    console.log(event.target.value);
-    this.setState({ completed: !this.state.completed });
-  };
-
   render() {
-    const { id, completed, name } = this.props;
+    const { id, completed, name, handleChange } = this.props;
+    console.log("completed", completed);
     return (
       <li>
-        <input type="checkbox" id={id} checked={this.state.completed} onChange={this.handleChange} /> {name}
+        <input type="checkbox" id={id} checked={completed} onChange={(event) => handleChange(event)} /> {name}
       </li>
     );
   }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,15 +2,28 @@ import React from 'react';
 import TodoData from '../data/todo.json';
 import Item from './TodoItem.js';
 
+let rebuiltList = [...TodoData];
+const TodoList = () => {
 
-const TodoList = () => (
-  <ul>
-    {
-      TodoData.map((props) =>
-        (<Item  {...props} />)
-      )
-    }
-  </ul>
-)
+  const handleChange = (event) => {
+    console.log(event.target.id);
+    let clickedItem = rebuiltList.find(todo => { return todo.id == event.target.id });
+    clickedItem.completed = event.target.checked;
+    rebuiltList = [...rebuiltList, clickedItem]
+    console.log(rebuiltList);
+    console.log(clickedItem);
+  };
+  console.log("in todolist", rebuiltList);
+  return (
+    <ul>
+      {
+        rebuiltList.map((todoitem) =>
+          (<Item handleChange={handleChange} key={todoitem.id} id={todoitem.id} name={todoitem.name} completed={todoitem.completed} />)
+        )
+      }
+    </ul>
+  )
+}
+
 
 export default TodoList;
