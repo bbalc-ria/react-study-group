f405dabf60f66b42b35f9bef8e851a8d5e94019b
Bogdan Balc <bogdan.balc@gmail.com>
2019-07-11 15:09:09 +0300

    change todo list to class component to generate component update when list changes

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
-class Item extends React.Component {
-  render() {
-    const { id, completed, name, handleChange } = this.props;
-    console.log("completed", completed);
-    return (
-      <li>
-        <input type="checkbox" id={id} checked={completed} onChange={(event) => handleChange(event)} /> {name}
-      </li>
-    );
-  }
-}
-export default Item
+const Item = ({ id, completed, name, handleChange }) => (
+  <li>
+    <input
+      type="checkbox"
+      id={id}
+      checked={!completed}
+      onChange={event => handleChange(event)}
+    />
+    <label htmlFor={id}>{name}</label>
+  </li>
+);
+
+export default Item;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,28 +2,41 @@ import React from 'react';
 import TodoData from '../data/todo.json';
 import Item from './TodoItem.js';
 
-let rebuiltList = [...TodoData];
-const TodoList = () => {
-
-  const handleChange = (event) => {
-    console.log(event.target.id);
-    let clickedItem = rebuiltList.find(todo => { return todo.id == event.target.id });
-    clickedItem.completed = event.target.checked;
-    rebuiltList = [...rebuiltList, clickedItem]
-    console.log(rebuiltList);
-    console.log(clickedItem);
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { todoList: TodoData };
+  }
+  handleChange = event => {
+    let clickedItem = this.state.todoList.find(todo => {
+      return todo.id === parseInt(event.target.id, 10);
+    });
+    clickedItem.completed = !clickedItem.completed;
+    const index = this.state.todoList.indexOf(clickedItem);
+    this.setState({
+      todoList: [
+        ...this.state.todoList.slice(0, index),
+        clickedItem,
+        ...this.state.todoList.slice(index + 1),
+      ],
+    });
   };
-  console.log("in todolist", rebuiltList);
-  return (
-    <ul>
-      {
-        rebuiltList.map((todoitem) =>
-          (<Item handleChange={handleChange} key={todoitem.id} id={todoitem.id} name={todoitem.name} completed={todoitem.completed} />)
-        )
-      }
-    </ul>
-  )
-}
 
+  render() {
+    return (
+      <ul>
+        {this.state.todoList.map(todoItem => (
+          <Item
+            handleChange={this.handleChange}
+            key={todoItem.id}
+            id={todoItem.id}
+            name={todoItem.name}
+            completed={todoItem.completed}
+          />
+        ))}
+      </ul>
+    );
+  }
+}
 
 export default TodoList;
