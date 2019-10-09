380817aa976628510956175bc011003eea6d02cf
Beniamin Laslau <laslaubeniamin@gmail.com>
2019-10-09 21:59:27 +0300

    Remove unnecesary

--- src/App.js
+++ src/App.js
@@ -7,21 +7,6 @@ import data from './json/items.json';
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        
-      </header> */}
       <br/>
       <TodoMain items={data} />
     </div>
