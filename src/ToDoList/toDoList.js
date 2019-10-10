df51c2ae6159652e2c1e5957fbfdafa8017e2caf
sdolha <sdolha@RIA366>
2019-10-10 16:48:07 +0300

    Implement todolist as a function component and use hooks for state.

--- src/ToDoList/toDoList.js
+++ src/ToDoList/toDoList.js
@@ -1,107 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header/header';
 import ItemList from './ItemList/itemList';
 import Footer from './Footer/footer';
 
-class ToDoList extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          filterValue: 'All',
-          items: [{name:"name 1", complete:true},{name:"name 2"}]
-        };
+function ToDoList(props) {
+    const [filterValue, setFilterValue] = useState('All');
+    const [items, setItems] = useState([{name:"name 1", complete:true},{name:"name 2"}]);
 
-        this.handleItemAdded = this.handleItemAdded.bind(this);
-        this.handleItemRemoved = this.handleItemRemoved.bind(this);
-        this.handleFilterOptionChanged = this.handleFilterOptionChanged.bind(this);
-        this.handleClearCompleted = this.handleClearCompleted.bind(this);
-        this.handleItemCompleted = this.handleItemCompleted.bind(this);
-        this.handleCheckAll = this.handleCheckAll.bind(this);
-      }
-
-    handleItemAdded(itemName){
-        let stateUpdates = {
-            items: this.state.items.concat([{name: itemName}])
-        }
-
-        this.setState(stateUpdates)
+    function handleItemAdded(itemName){
+        let updatedItems = items.concat([{name: itemName}]);
+        setItems(updatedItems);
     }
 
-    handleItemRemoved(itemIndex){
-        let stateUpdates = {
-            items: this.state.items.filter(function(item, index)
+    function handleItemRemoved(itemIndex) {
+        let remainingItems = items.filter(function(item, index)
                 {
                     return index !== itemIndex
-                }
-            )
-        }
-
-        this.setState(stateUpdates)
+                });
+        setItems(remainingItems);
     }
 
-    handleFilterOptionChanged(filterValue) {
-        let stateUpdates = {
-            filterValue: filterValue
-        }
-
-        this.setState(stateUpdates)
+    function handleFilterOptionChanged(filterValue) {
+        setFilterValue(filterValue);
     }
 
-    handleItemCompleted(itemIndex) {
-        let stateUpdates = {
-            items: this.state.items.map(function(item, index)
-                {
-                    if (index === itemIndex){
-                        item.complete = !item.complete;
-                    }
+    function handleItemCompleted(itemIndex) {
+        let updatedItems = items.map(function(item, index)
+        {
+            if (index === itemIndex){
+                item.complete = !item.complete;
+            }
 
-                    return item;
-                }
-            )
-        }
+            return item;
+        });
 
-        this.setState(stateUpdates)
+        setItems(updatedItems);
     }
 
-    handleClearCompleted(itemIndex) {
-        let stateUpdates = {
-            items: this.state.items.filter(function(item, index)
-                {
-                    return !item.complete;
-                }
-            )
-        }
-
-        this.setState(stateUpdates)
+    function handleClearCompleted(itemIndex) {
+        let remainingItems = items.filter(function(item, index)
+        {
+            return !item.complete;
+        });
+        
+        setItems(remainingItems);
     }
 
-    handleCheckAll(checkAll){
-        let stateUpdates = {
-            items: this.state.items.map(function(item, index)
-                {
-                    item.complete = checkAll;
-                    return item;
-                }
-            )
-        }
-
-        this.setState(stateUpdates)
+    function handleCheckAll(checkAll) {
+        let updatedItems = items.map(function(item, index)
+        {
+            item.complete = checkAll;
+            return item;
+        });
+        setItems(updatedItems);
     }
     
-    render() {
         return (
             <div>
-                <Header onItemAdded={this.handleItemAdded}
-                    onCheckAll={this.handleCheckAll} />
-                <ItemList items={this.state.items} filterValue={this.state.filterValue} 
-                    onItemRemoved={this.handleItemRemoved}
-                    onItemCompleted={this.handleItemCompleted} />
-                <Footer items={this.state.items} filterValue={this.state.filterValue} 
-                    onFilterOptionChanged={this.handleFilterOptionChanged} 
-                    onClearCompleted={this.handleClearCompleted} />
+                <Header onItemAdded={handleItemAdded}
+                    onCheckAll={handleCheckAll} />
+                <ItemList items={items} filterValue={filterValue} 
+                    onItemRemoved={handleItemRemoved}
+                    onItemCompleted={handleItemCompleted} />
+                <Footer items={items} filterValue={filterValue} 
+                    onFilterOptionChanged={handleFilterOptionChanged} 
+                    onClearCompleted={handleClearCompleted} />
             </div>
-      );
-    }
+        );
 }
 
 export default ToDoList;
