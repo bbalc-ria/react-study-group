c366524d8107b2e832882d319eac9f09ebbc4446
Filipoiu George <filipoiugeorge98@gmail.com>
2019-11-14 12:59:07 +0200

    added some svg markers

--- src/components/SearchPage/Marker.js
+++ src/components/SearchPage/Marker.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import * as S from './SearchPageStyle';
 import { ReactComponent as PinSvg } from '../../resources//pin.svg';
-import { ReactComponent as HomeSvg } from '../../resources//homeA.svg';
+import { ReactComponent as HomeSvg } from '../../resources//HomeAv2.svg';
 import { ReactComponent as SelectedSvg } from '../../resources//selected.svg';
 export default function Marker(props) {
 
@@ -9,7 +9,7 @@ export default function Marker(props) {
     <S.Marker>
       {
         !props.selected && (
-          props.home === true ? (<HomeSvg height="50px" width="50px"></HomeSvg>)
+          props.home === true ? (<HomeSvg height="100px" width="80px"></HomeSvg>)
             : (<PinSvg height="15px" width="15px"></PinSvg>))
       }
       {props.selected === true && <SelectedSvg height="25px" width="25px"></SelectedSvg>}
