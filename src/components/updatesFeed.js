717b223dbd277ef3e2f3e32244814cbfde67bd12
sdolha <sdolha@RIA366>
2019-11-14 13:17:54 +0200

    Add call to google books api using axios to display updates.

--- src/components/updatesFeed.js
+++ src/components/updatesFeed.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 import * as S from "../styles";
 import UpdatesFeedItem from "./updatesFeedItem"
-import { books } from "../data/books";
-
+//import { books } from "../data/books";
 
 function UpdatesFeed(props) {
+  const [books, setBooks] = useState([]);
+  
+  useEffect(() => {
+    Axios
+     .get(
+       "https://www.googleapis.com/books/v1/volumes?q=test"
+       , { crossdomain: true }
+     )
+     .then(({ data }) => {
+       console.log(data);
+      //setBooks(data);
+     });
+  }, [])
 
   function getFeedItems() {
+
         let feedItems = books.map((book, index) => 
             <UpdatesFeedItem book={book} key={index} />
         );
